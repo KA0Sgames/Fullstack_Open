537474c64eeffc4b0a28c6e8f7f726ff6716eca3
Joonas Ylonen <KA0Screativity@gmail.com>
2025-11-10 19:23:48 +0200

    Add exercise 2.4 kurssitiedot step9 and 2.5 erillinen moduuli

--- Osa2/kurssitiedot/src/App.jsx
+++ Osa2/kurssitiedot/src/App.jsx
@@ -11,30 +11,52 @@ import Course from './components/Course'
 } */
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <div>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
+    </div>
   )
 }
 
